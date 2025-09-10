ad10a2fd1247ffd262163ff01bc95f493641b431
Tyler VanAlstine <113937059+tylervanalstine1@users.noreply.github.com>
2025-09-10 18:03:48 -0500

    Move Firebase config to environment variables and update .gitignore

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
-// Replace with your Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyCdmFRZ-YByrchTQcKMYMzz4Hi2Qmu4ZGI",
-  authDomain: "cookbook-cdf05.firebaseapp.com",
-  projectId: "cookbook-cdf05",
-  storageBucket: "cookbook-cdf05.firebasestorage.app",
-  messagingSenderId: "865167617910",
-  appId: "1:865167617910:web:a5f46a5764c4c1643c5457",
-  measurementId: "G-N32QBJJ65P"
-};
+require('dotenv').config();
 
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
 
 console.log('FIREBASE CONFIG:', firebaseConfig);
 firebase.initializeApp(firebaseConfig);
